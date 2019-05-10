622764ad163d03d54c041293758e218ae99ee5eb
yang zhixiang <641383539@qq.com>
2019-05-10 08:33:24 +0000

    用户显示页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
- <head> 
-    <title>@yield('title', 'laravel.test') - Laravel 新手入门教程</title>
- </head>
- <body>
-   @yield('content')
- </body>
+  <head>
+    <title>@yield('title', 'Laravel.test') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
+
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
 </html>
